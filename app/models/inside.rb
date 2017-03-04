1ddede96faf36e4c24c352ab0e0c2cd5302e2eb5
mariocsee <mariocsee@gmail.com>
2017-03-04 17:36:25 -0500

    include season in search

--- app/models/inside.rb
+++ app/models/inside.rb
@@ -1,5 +1,5 @@
 class Inside < ApplicationRecord
   def self.search(search)
-    where("name LIKE ? OR category LIKE ? OR deadline LIKE ? OR department LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+    where("name LIKE ? OR category LIKE ? OR deadline LIKE ? OR department LIKE ? OR season LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
   end
 end
